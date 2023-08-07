9277c36b903adfab5c1f8da5f42a9f5b4df8417a
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-07 19:22:05 +0300

    Update HeroBanner.jsx

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,7 +1,13 @@
 import React from "react"
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+      </div>
+    </div>
+  )
 }
 
 export default HeroBanner
