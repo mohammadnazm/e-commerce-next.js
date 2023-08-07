9aa1383d0f31872e3782c4990d466ffd7f53a942
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-07 16:33:00 +0300

    Update schema.js

--- sanity_ecommerce/schemas/schema.js
+++ sanity_ecommerce/schemas/schema.js
@@ -1,9 +1,10 @@
 import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
-import customSchemaTypes from './schemas'
+import product from './product'
+import banner from './banner'
 
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat(customSchemaTypes),
+  types: schemaTypes.concat([product, banner]),
 })
