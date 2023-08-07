5322ba3c6af840a30b0fafa9a3a6e87839a01494
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-07 17:46:55 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <></>
+import React from "react"
+
+const Home = () => {
+  return (
+    <>
+      HeroBanner
+      <div>
+        <h2>Best Selling Products</h2>
+        <p>Speakers Of Many Variations</p>
+      </div>
+    </>
+  )
 }
+
+export default Home
