01822fbe32638aa92a90a35bb1dd5e5bfe03a529
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-07 20:22:54 +0300

    Update client.js

--- src/lib/client.js
+++ src/lib/client.js
@@ -2,9 +2,10 @@ import sanityCli from "@sanity/client"
 import ImageUrlBuilder from "@sanity/image-url"
 
 const client = sanityClient({
-  projectId: "",
-  dataset: "",
-  apiVersion: "",
+  projectId: "n67uv68h",
+  dataset: "production",
+  apiVersion: "2023-08-07",
   useCdn: true,
-  token: "",
+  token:
+    "skQKer68Nk0eBj6zsIVwP1pqbgzbgEts4Jv2lJSQTK7sVLhdUgF71ovAt96d6FuPPDKD5YEEZDnc5kzDt67AVfmVrB9ndCAuR4JCeg0I7e9S0LsaVs6TML7qjMksXR8na7n1RXnPqf78uaoDo1t9A3EzuO1Ju0Tvr6gw26IR16LxjZngOCKc",
 })
