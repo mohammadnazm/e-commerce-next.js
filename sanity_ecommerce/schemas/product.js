ad6ac92721d5ff3345ce5d81a315f965375c6e5c
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-07 16:14:05 +0300

    Update product.js

--- sanity_ecommerce/schemas/product.js
+++ sanity_ecommerce/schemas/product.js
@@ -8,6 +8,23 @@ export default {
       title: 'Image',
       type: 'array',
       of: [{type: 'image'}],
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 90,
+      },
     },
   ],
 }
