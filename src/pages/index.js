74319a8f77f5f2cae905b5afc1e748391d710c15
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-07 17:48:40 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,11 @@ const Home = () => {
         <h2>Best Selling Products</h2>
         <p>Speakers Of Many Variations</p>
       </div>
+      <div>
+        {["Product 1", "Product 2"].map(product => {
+          product
+        })}
+      </div>
     </>
   )
 }
