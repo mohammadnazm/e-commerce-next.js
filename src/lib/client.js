868db2e57239f70260a36987d44171dfc95842f2
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-07 20:19:58 +0300

    add SanityClient

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,2 +1,10 @@
 import sanityCli from "@sanity/client"
 import ImageUrlBuilder from "@sanity/image-url"
+
+const client = sanityClient({
+  projectId: "",
+  dataset: "",
+  apiVersion: "",
+  useCdn: true,
+  token: "",
+})
