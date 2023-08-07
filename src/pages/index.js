8839dd7b4402c677f3f6ed71ba7d02a3b26fb735
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-07 23:58:20 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,9 @@ const Home = ({ products, bannerData }) => {
         <p>Speakers Of Many Variations</p>
       </div>
       <div className="products-container">
-        {products?.map(product => product.name)}
+        {products?.map(product => (
+          <Product key={product.id} product={product} />
+        ))}
       </div>
       <FooterBanner />
     </>
