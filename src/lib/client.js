4b19e88b854fdf56198bfd91d001f317594fd072
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-07 20:27:14 +0300

    add environment variable

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,11 +1,14 @@
 import sanityCli from "@sanity/client"
 import ImageUrlBuilder from "@sanity/image-url"
 
-const client = sanityClient({
+export const client = sanityClient({
   projectId: "n67uv68h",
   dataset: "production",
   apiVersion: "2023-08-07",
   useCdn: true,
-  token:
-    "skQKer68Nk0eBj6zsIVwP1pqbgzbgEts4Jv2lJSQTK7sVLhdUgF71ovAt96d6FuPPDKD5YEEZDnc5kzDt67AVfmVrB9ndCAuR4JCeg0I7e9S0LsaVs6TML7qjMksXR8na7n1RXnPqf78uaoDo1t9A3EzuO1Ju0Tvr6gw26IR16LxjZngOCKc",
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 })
+
+const builder = ImageUrlBuilder(client)
+
+export const urlFor = source => builder.image(source)
