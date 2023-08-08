f55b54860731977e959f47dfa1e5d8b271d51f01
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-08 20:18:32 +0300

    add small images

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -15,22 +15,24 @@ const ProductDetails = ({ product, products }) => {
   return (
     <div>
       <div className="product-detail-container">
-        <div className="image-container">
-          <img
-            src={urlFor(image && image[index])}
-            className="product-detail-image"
-          />
-        </div>
-        <div className="small-images-container">
-          {image?.map((item, i) => (
+        <div>
+          <div className="image-container">
             <img
-              src={urlFor(item)}
-              className={
-                i === index ? "small-image selected-image" : "small-image"
-              }
-              onMouseEnter={() => setIndex(i)}
+              src={urlFor(image && image[index])}
+              className="product-detail-image"
             />
-          ))}
+          </div>
+          <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img
+                src={urlFor(item)}
+                className={
+                  i === index ? "small-image selected-image" : "small-image"
+                }
+                onMouseEnter={() => setIndex(i)}
+              />
+            ))}
+          </div>
         </div>
         <div className="product-detail-desc">
           <h1>{name}</h1>
