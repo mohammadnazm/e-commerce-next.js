7d55dd5629c1a7359b4d7a52723a0b18dfbb3deb
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-08 21:17:16 +0300

    Update StateContext.js

--- src/context/StateContext.js
+++ src/context/StateContext.js
@@ -14,7 +14,10 @@ export const StateContext = ({ children }) => {
   }
 
   const decQty = () => {
-    setQty(prevQty => prevQty - 1)
+    setQty(prevQty => {
+      if (prevQty - 1 < 1) return 1
+      return prevQty - 1
+    })
   }
 
   return (
