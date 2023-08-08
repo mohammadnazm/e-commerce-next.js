0d65f2ed2c08466ddc33d652c6fe042ddd645c37
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-08 18:58:20 +0300

    add track

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -58,7 +58,7 @@ const ProductDetails = ({ product, products }) => {
       </div>
       <div className="maylike-products-wrapper">
         <h2>You may also like</h2>
-        <div className="maylike-products-container">
+        <div className="maylike-products-container track">
           {products.map(item => (
             <Product key={item._id} product={item} />
           ))}
