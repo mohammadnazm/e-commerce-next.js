c58248a59cab6509f8e9f42b86cb213848007214
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-08 21:15:42 +0300

    add context provider

---
+++ src/context/StateContext.js
@@ -0,0 +1,27 @@
+import React, { createContext, useContext, useState, useEffect } from "react"
+
+import { toast } from "react-hot-toast"
+
+export const StateContext = ({ children }) => {
+  const [showCart, setShowCart] = useState(false)
+  const [cartItems, setCartItems] = useState()
+  const [totalPrice, setTotalPrice] = useState()
+  const [totalQuantities, setTotalQuantities] = useState()
+  const [qty, setQty] = useState(1)
+
+  const incQty = () => {
+    setQty(prevQty => prevQty + 1)
+  }
+
+  const decQty = () => {
+    setQty(prevQty => prevQty - 1)
+  }
+
+  return (
+    <Context.Provider
+      value={{ showCart, cartItems, totalPrice, totalQuantities, qty }}
+    >
+      {children}
+    </Context.Provider>
+  )
+}
