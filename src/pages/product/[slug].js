65bd8a8822d50883b2ee330bf11bb265c2c587d6
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-08 21:19:28 +0300

    Update [slug].js

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -7,6 +7,7 @@ import {
 } from "react-icons/ai"
 import { client, urlFor } from "../../lib/client"
 import { Product } from "@/components"
+import { useStateContext } from "../../context/StateContext"
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product
