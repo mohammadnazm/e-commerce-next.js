c093374d0aec5246b16a937690fa69902d292360
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-08 18:11:58 +0300

    Update [slug].js

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -6,6 +6,7 @@ import {
   AiOutlineStar,
 } from "react-icons/ai"
 import { client, urlFor } from "../../lib/client"
+import { Product } from "@/components"
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product
@@ -55,6 +56,14 @@ const ProductDetails = ({ product, products }) => {
               Buy Now
             </button>
           </div>
+        </div>
+      </div>
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="maylike-products-container">
+          {products.map(item => (
+            <Product key={item._id} product={item} />
+          ))}
         </div>
       </div>
     </div>
