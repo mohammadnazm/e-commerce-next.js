21610e25a4b32d473d0fad724c2e2dae68225429
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-09 22:39:05 +0300

    Update stripe.js

--- src/pages/api/stripe.js
+++ src/pages/api/stripe.js
@@ -5,6 +5,10 @@ export default async function handler(req, res) {
     try {
       // Create Checkout Sessions from body params.
       const params = {
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
