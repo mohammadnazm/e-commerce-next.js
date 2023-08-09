ddfed014c2542fcd82f5844b159cf8423ceb6d06
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-09 11:31:09 +0300

    add empty cart future

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -33,6 +33,16 @@ const Cart = () => {
           {cartItems.length < 1 && (
             <div className="empty-cart">
               <AiOutlineShopping size={150} />
+              <h3>Your shopping bag is empty</h3>
+              <Link href="/">
+                <button
+                  type="button"
+                  onClick={() => setShowCart(false)}
+                  className="btn"
+                >
+                  Continue Shopping
+                </button>
+              </Link>
             </div>
           )}
         </div>
