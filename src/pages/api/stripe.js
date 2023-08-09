4418cd5c3cbf7d5f9e5fa2697c0cc3ae4df60851
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-09 22:36:13 +0300

    upgrade

--- src/pages/api/stripe.js
+++ src/pages/api/stripe.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create({
+      const params = {
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
@@ -15,7 +15,8 @@ export default async function handler(req, res) {
         mode: "payment",
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
-      })
+      }
+      const session = await stripe.checkout.sessions.create(params)
       res.redirect(303, session.url)
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message)
