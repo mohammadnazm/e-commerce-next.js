0704d491676ca101abc59543f63c0d8191ad7e3d
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-09 19:45:18 +0300

    Update StateContext.js

--- src/context/StateContext.js
+++ src/context/StateContext.js
@@ -37,7 +37,7 @@ export const StateContext = ({ children }) => {
   const toggleCartItemQuantity = (id, value) => {
     foundProduct = cartItems.find(item => item._id === id)
     index = cartItems.findIndex(product => product._id === id)
-    const newCartItems = cartItems.splice(index, 1)
+    const newCartItems = cartItems.filter(item => item._id !== id)
 
     if (value === "inc") {
       setCartItems([
